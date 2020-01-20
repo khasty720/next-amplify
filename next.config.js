ffc96842f304b3cb23f8632a49a54debbe8d653c
Kyle Hasty <khasty@tenable.com>
2020-01-19 23:12:34 -0500

    Remove next-manifest

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
-const withManifest = require('next-manifest');
-const defaults = {
-  // next-manifest options
-  output: './public/', // The folder where the manifest will be generated.
-  // manifest options
-  name: 'PWA',
-  icons: [
-    {
-      "src": "/static/icons/icon-192x192.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "/static/icons/icon-512x512.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
-}
-module.exports = withManifest({
-  manifest: {
-    ...defaults
-  }
-});
+const withSass = require('@zeit/next-sass')
+
+module.exports = withSass({
+  cssModules: true
+})
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Manifest from 'next-manifest/manifest';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <Manifest 
-            href='manifest.json'
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
