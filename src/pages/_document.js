e26e8d43b52c7b04e6b0f14cd57999f4b2709817
Kyle Hasty <khasty@tenable.com>
2020-01-20 14:23:38 -0500

    Update Document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { JS } from 'aws-amplify';
+
+JS.browserOrNode = () => {
+  const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+  const isNode = typeof process !== 'undefined' && process.versions != null && process.versions.node != null;
+  return {
+    isBrowser,
+    isNode,
+  };
+};
 
 class MyDocument extends Document {
   render() {
